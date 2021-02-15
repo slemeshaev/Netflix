2773e6e0995461282962fe3b5341456429e1d7c5
slemeshaev <lemeshaevs@gmail.com>
2021-02-15 11:56:35 +0300

    files for project netflix

---
+++ Netflix/Netflix/NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixApp.swift
+//  Netflix
+//
+//  Created by Станислав Лемешаев on 15.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
