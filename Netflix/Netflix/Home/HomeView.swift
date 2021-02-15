e31f92fb0a6c6e80770e1f8359020d2771723874
slemeshaev <lemeshaevs@gmail.com>
2021-02-15 12:05:14 +0300

    file homeview

---
+++ Netflix/Netflix/Home/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Netflix
+//
+//  Created by Станислав Лемешаев on 15.02.2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
